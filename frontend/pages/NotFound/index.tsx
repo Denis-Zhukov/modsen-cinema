90ae3ff68b57541c84ebb8c956971dbfd2345f3e
Denis-Zhukov <DenisZhukov.Hachiko@gmail.com>
2023-07-24 13:49:02 +0300

    page is created

---
+++ frontend/pages/NotFound/index.tsx
@@ -0,0 +1,8 @@
+import styled from './style.module.scss';
+
+export const NotFound = () => (
+    <div className={styled.notFound}>
+        <h1>404</h1>
+        <p>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
+    </div>
+);
