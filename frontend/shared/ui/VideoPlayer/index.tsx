038555d65690c9a52d93f96ab4a1593c6d4564fe
Denis-Zhukov <DenisZhukov.Hachiko@gmail.com>
2023-07-31 05:17:09 +0300

    feat: videoplayer create

---
+++ frontend/shared/ui/VideoPlayer/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import 'plyr-react/plyr.css';
+import './video-player-styles.css';
+
+import Plyr from 'plyr-react';
+
+import { StyledVideoWrapper } from './styled';
+
+type Props = {
+    src: string
+    provider?: 'html5' | 'youtube'
+    preview?: boolean,
+};
+
+const defaultOptions = {
+    controls: ['play-large', 'play', 'progress', 'current-time', 'volume', 'mute', 'captions', 'settings', 'fullscreen'],
+    settings: ['quality', 'speed', 'loop'],
+    keyboard: {
+        global: false,
+        focused: true,
+    },
+    storage: { enabled: true, key: 'player' },
+};
+
+const previewOptions = {
+    controls: ['play-large'],
+    settings: [],
+    keyboard: {
+        global: false,
+        focused: false,
+    },
+};
+
+export const VideoPlayer = ({ src, provider, preview }: Props) => (
+    <StyledVideoWrapper>
+        <Plyr
+            source={{
+                type: 'video',
+                sources: [{ src, provider }],
+            }}
+            options={preview ? previewOptions : defaultOptions}
+        />
+    </StyledVideoWrapper>
+);
+
+VideoPlayer.defaultProps = {
+    provider: 'html5',
+    preview: false
+};
---
+++ frontend/shared/ui/VideoPlayer/styled.ts
@@ -0,0 +1,12 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const StyledVideoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  background: gray;
+  height: 500px;
+  overflow: hidden;
+`;
