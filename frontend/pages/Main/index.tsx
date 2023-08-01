504baa853c18d466cc40a90a89452d54bcffee6f
Denis-Zhukov <DenisZhukov.Hachiko@gmail.com>
2023-08-01 08:22:21 +0300

    feat(main page): studios is added

--- frontend/pages/Main/index.tsx
+++ frontend/pages/Main/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { StyledMain } from '@/pages/Main/styled';
+import { StyledMain, StyledYearText } from '@/pages/Main/styled';
+import { poppinsFont } from '@/shared/fonts';
+import { Studios } from '@/shared/ui/Studios';
 import { ShowNow } from '@/widgets/ui/ShowNow';
 import { Trailer } from '@/widgets/ui/Trailer';
 
@@ -8,5 +10,9 @@ export const Main = () => (
     <StyledMain>
         <Trailer/>
         <ShowNow/>
+        <div>
+            <Studios/>
+            <StyledYearText className={poppinsFont.className}>2023</StyledYearText>
+        </div>
     </StyledMain>
 );
--- frontend/pages/Main/styled.ts
+++ frontend/pages/Main/styled.ts
@@ -3,5 +3,15 @@ import styled from 'styled-components';
 export const StyledMain = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 85px;
+  gap: 100px;
+`;
+
+export const StyledYearText = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  font-weight: 300;
+  color: #FFFFFF;
+  padding: 20px;
 `;
